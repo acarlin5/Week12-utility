f48250e48ef00a9fb20bc0da78196c6489ca236d
Abby Carlin <acarlin@mymail.mines.edu>
2019-11-16 14:37:52 -0700

    created loadfile

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,13 @@
+# https://github.com/acarlin5/Week12-utility.git
+# Abby Carlin
+# CSCI 102 - Section C
+# Week 11 - Part B
+
 def PrintOutput(string):
     print('OUTPUT', string)
+
+def LoadFile(file):
+    with open(file, 'r') as myfile:
+        lines = myfile.readlines()
+        print("OUTPUT", lines)
+    
